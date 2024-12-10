f3963d5ca2cf73a62272fbc233a262cd34e9a085
napling <matthewcellis98@gmail.com>
2024-12-09 18:45:42 -0600

    commented out process env soo that server can start

--- server/server.js
+++ server/server.js
@@ -28,13 +28,14 @@ const startApolloServer = async () => {
     context: authMiddleware
   }));
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+  // if (process.env.NODE_ENV === 'production') {
+  // do we need this???
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  }
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+  // }
 
   db.once('open', () => {
     app.listen(PORT, () => {
