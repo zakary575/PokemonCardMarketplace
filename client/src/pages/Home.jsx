e69a91d5689e6c2c67a6af295316d216e947ad46
napling <matthewcellis98@gmail.com>
2024-12-09 21:22:39 -0600

    added jsx files and pages files

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Cart from "../components/Cart";
+import PokeCard from '../components/PokeCard'
+
+const Home = () => {
+  return (
+    <div className="container">
+      <Cart />
+      <PokeCard />
+    </div>
+  );
+};
+
+export default Home;
