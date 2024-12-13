cae6cc23e297965cb70885931fa531a245e5e7c0
napling <matthewcellis98@gmail.com>
2024-12-12 19:14:27 -0600

    added ssearchbar to homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Cart from "../components/Cart";
+import SearchBar from "../components/SearchBar";
 // import PokeCard from '../components/PokeCard'
 
 const Home = () => {
   return (
     <div className="container">
+      <SearchBar />
       <Cart />
     </div>
   );
